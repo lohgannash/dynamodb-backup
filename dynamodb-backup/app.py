7fb2ddd0a344aa2a6a1071ee90516b6b4762910e
Leonard Zou <l.zou@base2services.com>
2020-07-28 10:48:33 +1000

    update s3 bucket path convention

--- dynamodb-backup/app.py
+++ dynamodb-backup/app.py
@@ -107,7 +107,7 @@ def backup_table(bucket_path, table_name, frequency):
     paginator = client.get_paginator('scan')
     page_iterator = paginator.paginate(TableName=table_name, Select='ALL_ATTRIBUTES', ConsistentRead=True, PaginationConfig={'PageSize': 100})
     fs = s3fs.S3FileSystem()
-    s3_path = f'{BucketName}/{bucket_path}/{table_name}-{frequency}.json'
+    s3_path = f'{BucketName}/{table_name}/{bucket_path}/{table_name}-{frequency}-{bucket_path}.json'
     with fs.open(s3_path, 'w') as f:
         for page in page_iterator:
             for item in page['Items']:
@@ -158,7 +158,7 @@ def lambda_handler(event, context):
 
         if event["action"] == "backup-table":
             if "table_name" in event:
-                bucket_path = dt.strftime('%Y-%m-%d/%H.%M.%S') # formats to '2020-12-31/23.59.59'
+                bucket_path = dt.strftime('%Y-%m-%d-%H-%M-%S') # formats to '2020-12-31-23-59-59'
                 backup_table(bucket_path, event["table_name"], frequency)
     else:
         raise Exception("An 'action' is missing from this invocations payload")
